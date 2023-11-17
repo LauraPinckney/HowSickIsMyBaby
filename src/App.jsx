b47efad6a206d560991fc899c9b73e548b53418c
Laura Pinckney <laura.pinckney@yahoo.com>
2023-11-17 15:11:24 +0000

    addition of page to app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import NotFound from './pages/NotFoundPage/NotFound';
 
 export const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
