0fa5f59e6ffcf771452832f7c18ba1fa51b7be8d
Laura Pinckney <laura.pinckney@yahoo.com>
2023-11-21 19:43:23 +0000

    setting state and api bits for answers

--- src/components/QuestionAnswers/QuestionAnswers.jsx
+++ src/components/QuestionAnswers/QuestionAnswers.jsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import styles from './QuestionAnswers.module.scss';
 import { NavButtons } from '../NavButtons/NavButtons';
+import axios from 'axios';
+import { BASE_URL } from '../../constants';
 
-export const QuestionAnswers = ({ questions }) => {
+const baseAnswersState = {
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0,
+  5: 0,
+  6: 0,
+  7: 0,
+  8: 0,
+  9: 0,
+  10: 0,
+  11: 0,
+  12: 0,
+  13: 0,
+  14: 0,
+  15: 0,
+  16: 0,
+  17: 0,
+  18: 0,
+  19: 0,
+};
+
+export const QuestionAnswers = () => {
+  const [questions, setQuestions] = useState([]);
+  const [selectedAnswers, setSelectedAnswers] = useState(baseAnswersState);
+
+  const fetchQuestions = async () => {
+    const { data } = await axios.get(`${BASE_URL}/questions`);
+    setQuestions(data);
+  };
+
+  useEffect(() => {
+    fetchQuestions();
+  }, []);
+
   if (!questions.length) {
     return;
   }
-  // console.log(JSON.stringify(questions, null, 2));
 
   const qOne = questions[0];
   return (
@@ -15,18 +50,27 @@ export const QuestionAnswers = ({ questions }) => {
         <h2>{qOne.question}</h2>
         <h3>{qOne.further}</h3>
         <div className={styles.containerButtons}>
-          {qOne.answers.map((answer) => {})}
-          <label></label>
-          <input id="answer" type="radio" name="Answer" />
-          <label>
-            <input id="answer" type="radio" name="Answer" /> "a2"
-          </label>
-          <label>
-            <input id="answer" type="radio" name="Answer" /> "a3"
-          </label>
-          <label>
-            <input id="answer" type="radio" name="Answer" /> "a4"
-          </label>
+          {qOne.answers.map((answer, index) => {
+            return (
+              <Fragment key={answer.question}>
+                <label htmlFor={`question-${index + 1}`}>
+                  {answer.question}
+                </label>
+                <input
+                  id="answer"
+                  type="radio"
+                  name="Answer"
+                  value={`question-${index + 1}`}
+                  onClick={() => {
+                    setSelectedAnswers({
+                      ...selectedAnswers,
+                      [1]: answer.score,
+                    });
+                  }}
+                />
+              </Fragment>
+            );
+          })}
         </div>
       </div>
 
