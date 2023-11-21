906733c1f15d9edd216a86241446880ec9c1da36
Laura Pinckney <laura.pinckney@yahoo.com>
2023-11-21 14:15:51 +0000

    init of api

--- src/components/QuestionAnswers/QuestionAnswers.jsx
+++ src/components/QuestionAnswers/QuestionAnswers.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import styles from './QuestionAnswers.module.scss';
 
-export const QuestionAnswers = () => {
+export const QuestionAnswers = ({ questions }) => {
+  console.log(JSON.stringify(questions, null, 2));
   return (
     <div className={styles.container}>
       <h2>"question"</h2>
       <h3>"further"</h3>
       <div className={styles.containerButtons}>
         <label>
-          <input id="answer" type="radio" name="qAnswer" /> "a1"
+          <input id="answer" type="radio" name="Answer" /> "a1"
         </label>
         <label>
-          <input id="answer" type="radio" name="qAnswer" /> "a2"
+          <input id="answer" type="radio" name="Answer" /> "a2"
         </label>
         <label>
-          <input id="answer" type="radio" name="qAnswer" /> "a3"
+          <input id="answer" type="radio" name="Answer" /> "a3"
         </label>
         <label>
-          <input id="answer" type="radio" name="qAnswer" /> "a4"
+          <input id="answer" type="radio" name="Answer" /> "a4"
         </label>
       </div>
     </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.scss';
 import { Introduction, QuestionAnswers, NavButtons } from '../../components';
+import axios from 'axios';
+import { BASE_URL } from '../../constants';
 
 export const Home = () => {
+  const [questions, setQuestions] = useState([]);
+
+  const fetchQuestions = async () => {
+    const { data } = await axios.get(`${BASE_URL}/questions`);
+    setQuestions(data);
+  };
+
+  useEffect(() => {
+    fetchQuestions();
+  }, []);
+
   return (
     <div>
       <Introduction />
-      <QuestionAnswers />
+      <QuestionAnswers questions={questions} />
       <NavButtons />
     </div>
   );
