4c964139fe967e3944d23d942444fb21d51b07fd
Laura Pinckney <laura.pinckney@yahoo.com>
2023-11-24 15:13:56 +0000

    results api code

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,26 +1,30 @@
 import { useParams } from 'react-router-dom';
 import styles from './Results.module.scss';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { BASE_URL } from '../../constants';
 
 export const Results = () => {
   const { score } = useParams();
 
-  const [resultData, setResultData] = useState({});
+  const [resultData, setResultData] = useState('');
 
   const getResult = async () => {
     const { data } = await axios.post(`${BASE_URL}/result`, { score });
     setResultData(data);
   };
 
-  if (!Object.keys(resultData).length) {
+  useEffect(() => {
+    getResult();
+  }, []);
+
+  if (!resultData) {
     return;
   }
 
   return (
     <div className={styles.container}>
-      <h2>description from API</h2>
+      <h2>{resultData}</h2>
     </div>
   );
 };
