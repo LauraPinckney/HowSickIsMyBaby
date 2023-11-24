45b7628a6bbad33afbb4be8e66979bb4e5cc200f
Laura Pinckney <laura.pinckney@yahoo.com>
2023-11-24 14:42:19 +0000

    further work on api post request

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,7 +1,23 @@
+import { useParams } from 'react-router-dom';
 import styles from './Results.module.scss';
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { BASE_URL } from '../../constants';
 
 export const Results = () => {
+  const { score } = useParams();
+
+  const [resultData, setResultData] = useState({});
+
+  const getResult = async () => {
+    const { data } = await axios.post(`${BASE_URL}/result`, { score });
+    setResultData(data);
+  };
+
+  if (!Object.keys(resultData).length) {
+    return;
+  }
+
   return (
     <div className={styles.container}>
       <h2>description from API</h2>
