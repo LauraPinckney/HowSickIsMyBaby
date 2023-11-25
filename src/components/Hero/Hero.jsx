21f031a0cdfe1190bdb249a23f2739e3545f70d0
Laura Pinckney <laura.pinckney@yahoo.com>
2023-11-25 15:12:14 +0000

    update of hero image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,11 @@
 import styles from './Hero.module.scss';
 import React from 'react';
-import sketchBaby from '../../assets/images/sketchBaby.jpg';
-import heroImage from '../../assets/images/heroImage.jpg';
+import TabletHero from '../../assets/images/TabletHero.jpg';
 
 export const Hero = () => {
   return (
     <div className={styles.container}>
-      <img className={styles.image} src={heroImage} alt="Sketch of Baby"></img>
+      <img className={styles.image} src={TabletHero} alt="Sketch of Baby"></img>
     </div>
   );
 };
